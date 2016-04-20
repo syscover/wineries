e7830b337abdcab71d0feb92710d17391137bcf0
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-04-21 01:05:10 +0200

    refactor app

--- src/views/includes/nav/main.blade.php
+++ src/views/includes/nav/main.blade.php
@@ -1,8 +1,8 @@
-<li{!! Miscellaneous::setCurrentOpenPage(['wineries-winery']) !!}>
+<li{!! is_current_resource(['wineries-winery']) !!}>
     <a href="javascript:void(0)"><i class="fa fa-glass"></i>{{ trans('wineries::pulsar.package_name') }}</a>
     <ul class="sub-menu">
-        @if(session('userAcl')->allows('wineries-winery', 'access'))
-            <li{!! Miscellaneous::setCurrentPage('wineries-winery') !!}><a href="{{ route('winery', [session('baseLang')->id_001]) }}"><i class="fa fa-glass"></i>{{ trans_choice('wineries::pulsar.winery', 2) }}</a></li>
+        @if(is_allowed('wineries-winery', 'access'))
+            <li{!! is_current_resource('wineries-winery') !!}><a href="{{ route('winery', [session('baseLang')->id_001]) }}"><i class="fa fa-glass"></i>{{ trans_choice('wineries::pulsar.winery', 2) }}</a></li>
         @endif
     </ul>
 </li>
